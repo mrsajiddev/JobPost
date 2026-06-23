1940ce8d9556f416509d0417c83c2b7eefc01f12
Ahsan Ali <124239388+ahsanali-dev@users.noreply.github.com>
2026-06-24 02:42:44 +0500

    Update user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,7 +3,6 @@ import {
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
-  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity()
@@ -27,18 +26,11 @@ export class User {
   password!: string;
 
   @Column({ default: true })
-  is_active!: boolean;
+  is_verified!: boolean;
 
   @CreateDateColumn({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP(6)',
   })
   created_at!: Date;
-
-  @UpdateDateColumn({
-    type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP(6)',
-    onUpdate: 'CURRENT_TIMESTAMP(6)',
-  })
-  updated_at!: Date;
 }
